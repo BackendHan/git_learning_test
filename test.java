f15fd92c18e065d6d73eabbb98267fe506a30293
han <gmh926@bu.edu>
2024-12-28 19:58:11 -0500

    test file

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello meituan!");
         int a = 1;
-        System.out.println(a + " aaaa ");
+        System.out.println(a);
     }
 }
